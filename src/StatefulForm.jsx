9d172c6c3e216b7e878d149814db1964996eb67a
SI-Tanbir <si017490@gmail.com>
2024-11-20 09:10:13 +0600

    adding rules for longer password

--- src/StatefulForm.jsx
+++ src/StatefulForm.jsx
@@ -3,10 +3,22 @@ import { useState } from "react";
 
 const StatefulForm = () => {
     const [email,setEmail]=useState(null);
+    const [pass,setPass] =useState(null);
+    const [error,setError]=useState('');
+
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        console.log(email)
+        if(pass.length <6){
+            setError(' Password must longer than 6 ')
+            
+        }
+        else{
+            setError('');
+            console.log(email,pass)
+
+        }
+        
 
     }
 
@@ -16,6 +28,15 @@ const StatefulForm = () => {
         // console.log(e.target.value)
         
 
+
+    }
+
+    const passHandler =(e)=>{
+
+        setPass(e.target.value)
+        // console.log(e.target.value)
+            
+        
     }
 
   return (
@@ -24,8 +45,14 @@ const StatefulForm = () => {
 <form onSubmit={handleSubmit}>
 
 <input onChange={emailHandler} className='border border-red-600 rounded-md' type="text" name="name" />
+
+<br />
+<input onChange={passHandler} className='border border-red-600 rounded-md' type="password" name="password" />
 <br />
 <input className='border p-1 bg-slate-400  rounded-md' type="submit" value="submit" />
+{
+    error && <p>{error}</p>
+}
 </form>
 
     </div>
